9ae98aa9f95fff48b497c9ac284c0ab2feae5394
tsehsuan1102 <sam9883043@gmail.com>
2021-01-22 15:25:25 +0800

    run training

---
+++ run.sh
@@ -0,0 +1,17 @@
+# optimizer = Adadelta
+TASK_NAME='sst2'
+MODEL_NAME='bert-base-cased'
+
+
+
+python run_glue.py \
+    --model_name_or_path $MODEL_NAME \
+    --task_name $TASK_NAME \
+    --do_train \
+    --do_eval \
+    --max_seq_length 128 \
+    --per_device_train_batch_size 32 \
+    --learning_rate 2e-5 \
+    --num_train_epochs 3 \
+    --output_dir ./results/$TASK_NAME/
+
