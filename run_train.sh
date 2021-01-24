505eb3e4f0a56cec6c6bc8be1de14d58fe2fe036
b06902012 <b06902012@meow1.csie.ntu.edu.tw>
2021-01-24 17:45:37 +0800

    eval OK

--- run_train.sh
+++ run_train.sh
@@ -1,7 +1,8 @@
 export TASK_NAME=rte
+export POSTFIX=test
 
 CUDA_VISIBLE_DEVICES=1 python3 run_glue.py \
-  --model_name_or_path results/all/ \
+  --model_name_or_path bert-base-cased \
   --task_name $TASK_NAME \
   --do_train \
   --do_eval \
@@ -10,8 +11,8 @@ CUDA_VISIBLE_DEVICES=1 python3 run_glue.py \
   --learning_rate 2e-5 \
   --num_train_epochs 3 \
   --overwrite_output_dir \
-  --output_dir ./results/$TASK_NAME/
+  --output_dir ./results/${TASK_NAME}_${POSTFIX}/
 
 #Need to do this incase that the trainer load the state in finetuning for continue training
 #When continue training, modify the trainer_log.json back to trainer_state.json, and the model can continue training
-mv ./results/$TASK_NAME/trainer_state.json ./results/$TASK_NAME/trainer_log.json
+mv ./results/${TASK_NAME}_${POSTFIX}/trainer_state.json ./results/${TASK_NAME}_${POSTFIX}/trainer_log.json
