34152534e9118dd3ea86fd3a803f999ca82fc46b
b06902012 <b06902012@meow1.csie.ntu.edu.tw>
2021-01-24 17:41:20 +0800

    make sure finetune is able to run, try only eval

--- run_train.sh
+++ run_train.sh
@@ -1,7 +1,7 @@
-export TASK_NAME=all
+export TASK_NAME=rte
 
 CUDA_VISIBLE_DEVICES=1 python3 run_glue.py \
-  --model_name_or_path bert-base-cased \
+  --model_name_or_path results/all/ \
   --task_name $TASK_NAME \
   --do_train \
   --do_eval \
@@ -11,3 +11,7 @@ CUDA_VISIBLE_DEVICES=1 python3 run_glue.py \
   --num_train_epochs 3 \
   --overwrite_output_dir \
   --output_dir ./results/$TASK_NAME/
+
+#Need to do this incase that the trainer load the state in finetuning for continue training
+#When continue training, modify the trainer_log.json back to trainer_state.json, and the model can continue training
+mv ./results/$TASK_NAME/trainer_state.json ./results/$TASK_NAME/trainer_log.json
