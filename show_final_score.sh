651e1f350a798f54890b455ca2fde04d6a700315
billkunghappy <billkung.happy@gmail.com>
2021-03-16 01:53:08 +0800

    fix show_final_score

--- show_final_score.sh
+++ show_final_score.sh
@@ -1,5 +1,6 @@
 #Use this script to print results, the example is for TD-MTDNN(500) and TO-MTDNN(1000)
 #MODEL_NAME=all_rand500DCE_1000
 MODEL_NAME=$1
-python3 tools/print_score.py $MODEL_NAME --finetune --epoch_num 10
+python3 tools/print_loss.py $MODEL_NAME --finetune --epoch_num 5
+python3 tools/print_score.py $MODEL_NAME --finetune --epoch_num 5
 python3 tools/print_use_data.py $MODEL_NAME
